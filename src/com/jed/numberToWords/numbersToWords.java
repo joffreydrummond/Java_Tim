169190b51f4378eaa6ddd9dc3f08ecca487ba14e
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-13 14:32:35 -0700

    added zero to switch and modified if statement on line 9

--- src/com/jed/numberToWords/numbersToWords.java
+++ src/com/jed/numberToWords/numbersToWords.java
@@ -1,39 +1,58 @@
 package com.jed.numberToWords;
 
 public class numbersToWords {
-  public static void main(String[] args) {numberToWords(34);}
+  public static void main(String[] args) {
+    numberToWords(34034);
+  }
 
   public static void numberToWords(int number) {
-    if (number < 1) System.out.println("Invalid Value");
-    int lastDigitOfNumber = number % 10;
+    if (number < 0) System.out.println("Invalid Value");
 
-    for (int i = 0; i < number; i++){
+    for (int i = 0; i < number; i++) {
+      int lastDigitOfNumber = number % 10; //sets variable to last digit in the number
+      number /= 10; // sets number to the remaining digits
       switch (lastDigitOfNumber) {
-        case 1 : System.out.println("One");
+        case 0:
+          System.out.println("Zero");
           break;
-        case 2 : System.out.println("Two");
+        case 1:
+          System.out.println("One");
           break;
-        case 3 : System.out.println("Three");
+        case 2:
+          System.out.println("Two");
           break;
-        case 4 : System.out.println("Four");
+        case 3:
+          System.out.println("Three");
           break;
-        case 5 : System.out.println("Five");
+        case 4:
+          System.out.println("Four");
           break;
-        case 6 : System.out.println("Six");
+        case 5:
+          System.out.println("Five");
           break;
-        case 7 : System.out.println("Seven");
+        case 6:
+          System.out.println("Six");
           break;
-        case 8 : System.out.println("Eight");
+        case 7:
+          System.out.println("Seven");
           break;
-        case 9 : System.out.println("Nine");
+        case 8:
+          System.out.println("Eight");
           break;
-        default:
+        case 9:
+          System.out.println("Nine");
+          break;
+        default: break;
       }
-      lastDigitOfNumber--;
-      number = number/10;
-    }
 
-   }
+    }
+  }
+
+  //   public static int reverse(int number){
+  //    int reversed = 0;
+  //
+  //   }
+
 }
 
 /*
