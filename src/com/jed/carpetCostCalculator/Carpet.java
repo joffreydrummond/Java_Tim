3ab9631a1e537bd9b05a836abcdc356e36be6e32
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-06-12 15:12:23 -0700

    created carpet calculator. tested working

---
+++ src/com/jed/carpetCostCalculator/Carpet.java
@@ -0,0 +1,17 @@
+package com.jed.carpetCostCalculator;
+
+public class Carpet {
+
+
+
+
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+}
