ee0b9ffc74ef81af910a389a22326b47e3559954
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-06-12 19:35:58 -0700

    created PC using composition

---
+++ src/com/jed/composition/Monitor.java
@@ -0,0 +1,34 @@
+package com.jed.composition;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private  Resolution nativeResolution;
+
+public void drawPixelArt(int x, int y, String color){
+    System.out.println("Drawing pixel at " + x + ", " + y + " in color " + color);
+}
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
+}
